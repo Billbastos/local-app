e8c39307a4635b57404c5e5684b031d323b4f5f7
(Gui)lherme Bastos <gui.b.bastos@gmail.com>
2021-06-17 14:41:53 -0700

    external pages added

--- src/Contact.js
+++ src/Contact.js
@@ -6,6 +6,7 @@ const Contact = () => {
             <h1>Contact info</h1>
             <main>
                 <span><A target='blank' href='https://github.com/Billbastos'>github.com/Billbastos</A></span>
+                <span><A target='blank' href='pages/external'>leaving the react app 👋🏻</A></span>
             </main>
         </div>
     );
