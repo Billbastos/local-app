d43108b76db6e119c3d26bfec5f3596c9005ef84
(Gui)lherme Bastos <gui.b.bastos@gmail.com>
2021-06-17 10:26:11 -0700

    fix contact info

--- src/Contact.js
+++ src/Contact.js
@@ -3,9 +3,7 @@ const Contact = () => {
         <div>
             <h1>Contact info</h1>
             <div>
-                <span>#44-7345 sandborne Ave</span>
-                <span>Burnaby | BC</span>
-                <span>BV3N 4W6</span>
+                <span>https://github.com/Billbastos</span>
             </div>
         </div>
     );
