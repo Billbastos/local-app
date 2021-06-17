702bb056d1db8f350a386b96d889fdf42c637ed7
(Gui)lherme Bastos <gui.b.bastos@gmail.com>
2021-06-17 12:17:24 -0700

    refactoring styles to use Emotion

--- src/App.js
+++ src/App.js
@@ -1,42 +1,76 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import About from './About'
 import Contact from './Contact'
 import Home from './Home'
+import styled from '@emotion/styled'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Router basename="/local-app"> 
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <Switch>
-          <Route path="/about">
-            <About/>
-          </Route>
-          <Route path="/contact">
-            <Contact/>
-          </Route>
-          <Route path="/">
-            <Home/>
-          </Route>
-        </Switch>
+    <StyledApp>
+      <Router basename="/local-app">
+       
+        <Nav>
+          <Ul>
+            <Li>
+              <Link to="/" className='App-link'>Home</Link>
+            </Li>
+            <Li>
+              <Link to="/about" className='App-link'>About</Link>
+            </Li>
+            <Li>
+              <Link to="/contact" className='App-link'>Contact</Link>
+            </Li>
+          </Ul>
+        </Nav>
+        
+        <Main>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Main>
       </Router>
-    </div>
+      </StyledApp>
   );
 }
+
+const StyledApp = styled.div`
+  text-align: center;
+`
+const Main = styled.main`
+  margin: 20px 0;
+`
+
+const Nav = styled.nav`
+  background-color: #282c34;
+  min-height: 20vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Ul = styled.ul`
+  display: inline;
+  list-style: none;
+`
+
+const Li = styled.li`
+  display: inline;
+  margin: 0 10px;
+  a {
+    color: #61dafb;
+    text-decoration: none;
+  }
+`
 
 export default App;
