a4531bd79337cc854ff8d20f2f5d6fac786e5dfc
(Gui)lherme Bastos <gui.b.bastos@gmail.com>
2021-06-17 15:13:59 -0700

    open same tab for external html

--- src/Contact.js
+++ src/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
             <h1>Contact info</h1>
             <main>
                 <div><A target='blank' href='https://github.com/Billbastos'>github.com/Billbastos</A></div>
-                <div><A target='blank' href='../local-app/pages/external.html'>leaving the react app 👋🏻</A></div>
+                <div><A href='../local-app/pages/external.html'>leaving the react app 👋🏻</A></div>
             </main>
         </div>
     );
